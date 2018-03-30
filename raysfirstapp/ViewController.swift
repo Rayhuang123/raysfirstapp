48c9e6e68088cacbd303f812472539c7f971f364
weiping Huang <superpig881@gmail.com>
2018-03-30 20:25:52 +0800

    Initial Commit

---
+++ raysfirstapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  raysfirstapp
+//
+//  Created by Ray Huang on 2018/3/30.
+//  Copyright © 2018年 Ray Huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
